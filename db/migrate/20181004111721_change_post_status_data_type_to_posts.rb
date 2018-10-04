6a1686bc4cb5d59852e1c1cf4fb0c7d8c95c29b5
Joana Palero <joana.palero24@gmail.com>
2018-10-04 11:22:13 +0000

    automatically backed up by learn

--- db/migrate/20181004111721_change_post_status_data_type_to_posts.rb
+++ db/migrate/20181004111721_change_post_status_data_type_to_posts.rb
@@ -1,5 +1,5 @@
 class ChangePostStatusDataTypeToPosts < ActiveRecord::Migration
   def change
-    add_column :posts, :post_status, :boolean
+    add_column :posts, :post_status, :string
   end
 end
